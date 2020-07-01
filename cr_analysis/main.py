4aef928741b2f12dddad6591cdf1f7b6b3162d66
TOOOOOOMY <45617592+TOOOOOOMY@users.noreply.github.com>
2020-07-01 19:49:25 +0900

    Update main.py

--- cr_analysis/main.py
+++ cr_analysis/main.py
@@ -1,8 +1,8 @@
 """
 ==========
 Author: Tomoki WATANABE
-Update: 10/06/2020
-Version: 2.0.1
+Update: 01/07/2020
+Version: 3.0.0
 License: BSD License
 Programing Language: Python3
 ==========
@@ -13,11 +13,14 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+# graph_settings
 settings = {
     "x_axis_title" : "Time [h]",
     "y_axis_title" : "Bioluminescence",
-    "column_number" : 2,
+    "column_number" : 5,
+    "overwrite_column_number" : 4,
     "y_axis_share_switch" : 1,
+    "yaxis_percentage_switch" : 1,
     "over_view_image_length" : 4.5,
     "over_view_image_width" : 5.0,
     "all_plot_column_number" : 12,
@@ -27,39 +30,51 @@ settings = {
 
 
 settings_2 = {
-    0 : ["black", "Group0"],
-    1 : ["red", "Group1"],
-    2 : ["orange", "Group2"],
-    3 : ["green", "Group3"],
-    4 : ["lightgreen", "Group4"],
-    5 : ["blue", "Group5"],
+    0 : ["black", "Non"],
+    1 : ["red", "TOC1-Resi-R"],
+    2 : ["orange", "TOC1-Resi-AR"],
+    3 : ["green", "TOC1-Sens-R"],
+    4 : ["lightgreen", "TOC1-Sens-AR"],
+    5 : ["blue", "CBR"],
     6 : ["lightblue", "Group6"],
-    7 : ["yellow", "Group7"],
-    8 : ["teal", "Group8"],
-    9 : ["cyan", "Group9"],
-    10 : ["gray", "Group10"],
+    7 : ["orangered", "PRR1-Resi-R"],
+    8 : ["salmon", "PRR1-Resi-AR"],
+    9 : ["forestgreen", "PRR1-Sens-R"],
+    10 : ["limegreen", "PRR1-Sens-AR"],
 }
+
+
+setting_3 = {}
+
+
 
 def visualizer(file_name, # include .csv -> sample.csv
                file_path = "", # /content/sample.csv -> /content/
                graph_settings = settings,
                subtitle_and_color = settings_2,
-               file_from = 1,
+               overlap_dict = setting_3,
+               file_from = 1, #Lumicecの場合は0
                sampling_period = 60,
                estimated_period = 24,
                over_view_plot_switch = 1,
                 all_plot_switch = 1
-):
-    ##x軸単位（「"」に囲まれた部分を書き換え、以下同じ）
+    ):
+
     x_axis_title = graph_settings["x_axis_title"]
-    ##y軸単位
+
     y_axis_title = graph_settings["y_axis_title"]
 
-    ##Overview plotでのグラフ横列表示数（１～５、標準３）
+    ##Overview以外のplotでのグラフ横列表示数（１～５、標準３）
     column_number = graph_settings["column_number"]
+
+    ##Overview plotでのグラフ横列表示数（１～５、標準３）
+    overwrite_column_number = graph_settings["overwrite_column_number"]
 
     ##All plotでのグラフ間Y軸共有の有無（共有無→０、有→１）
     y_axis_share_switch = graph_settings["y_axis_share_switch"]
+
+    ##６-２：Y軸を各データの最大値に対する%とする場合は１
+    yaxis_percentage_switch = graph_settings["yaxis_percentage_switch"]
 
     ##グループ番号毎の色（参考：https://matplotlib.org/examples/color/named_colors.html）、先頭から数字の０に対応
 
@@ -77,7 +92,9 @@ def visualizer(file_name, # include .csv -> sample.csv
     ###画像1枚あたりの横サイズ
     a_width = graph_settings["all_plot_image_width"]
 
-
+    """
+    Support functions
+    """
     def color_changer(data_number):
         COLOR = subtitle_and_color[int(data_number)][0] # color_list[int(data_number)]
         Subtitle = subtitle_and_color[int(data_number)][1] # subtitle_list[int(data_number)]
@@ -118,46 +135,28 @@ def visualizer(file_name, # include .csv -> sample.csv
         return ROW, Col
 
 
-    def router(file_path, file_name, column_number):
-        if file_name[-4:] != ".csv":
-            print("ERROR ->\nPlease use csv file.")
-            sys.exit()
-        row_data = pd.read_csv("{0}{1}".format(file_path, file_name), engine="python", encoding="utf-8_sig")
-        try:
-            new_data = row_data.drop('Unnamed: 0', axis=1).T
-            X_axis = round(row_data["Unnamed: 0"].iloc[1:].reset_index(drop=True).astype(float), 4)
-        except KeyError:
-            new_data = row_data.drop('Time', axis=1).T
-            X_axis = round(row_data['Time'].iloc[1:].reset_index(drop=True).astype(float), 4)
-        finally:
-            try:
-                all_data = new_data.drop(0, axis=1).T.reset_index(drop=True)
-                data_name = file_name
-            except:
-                print("<==========\nCSV file Error. \n\nA1 cell needs to be 'Time' or blank.\n==========>")
-                sys.exit()
-            else:
-                if y_axis_share_switch == 0:
-                    Yaxis = "Not shared"
-                else :
-                    Yaxis = "Y shared"
-
-                if over_view_plot_switch == 1:
-                    colored_overview_n_columns(X_axis, new_data, all_data, data_name, Yaxis, column_number, x_axis_title, y_axis_title)
-                else:
-                    pass
-
-                if all_plot_switch == 1:
-                    all_plot(X_axis, new_data, all_data, data_name, Yaxis)
-                else:
-                    pass
+    def percentage_data_transfer(shaped_data):
+        cnt_list = []
+        for col in shaped_data.T.columns:
+            # print(col)
+            target_data = shaped_data.drop(0, axis=1).T[col]
+            cnt_list.append([shaped_data.T[col][0]]+list(target_data/max(target_data)*100))
+        return pd.DataFrame(cnt_list, columns=shaped_data.T.index, index=shaped_data.T.columns)
 
 
-    def colored_overview_n_columns(X_axis, new_data, all_data, data_name, Yaxis, n, x_axis_title, y_axis_title):
-        group_list = sorted(list(set(new_data[0])))
+    """
+    main functions
+    """
+    def colored_overview_n_columns(X_axis, shaped_data, data_name, Yaxis, n, x_axis_title, y_axis_title):
+        group_list = sorted(list(set(shaped_data[0])))
 
-        F_max = np.amax(np.amax(all_data))
-        Y_max = -(-F_max//1000)*1000
+        if yaxis_percentage_switch == 1:
+            new_data = percentage_data_transfer(shaped_data)
+            Y_max = 100
+        else :
+            new_data = shaped_data
+            F_max = np.amax(np.amax(new_data.drop(0, axis=1)))
+            Y_max = -(-F_max//1000)*1000
 
         fig = plt.figure(figsize=(n*ov_width, -(-(len(group_list)+n)//n)*ov_length))
         for I in range (0, len(group_list)+1, 1):
@@ -170,12 +169,13 @@ def visualizer(file_name, # include .csv -> sample.csv
                     ax =  fig.add_subplot(-(-(len(group_list)+n)//n), n, I + n)
 
             if I == 0:
-                process_data = all_data
+                process_data = new_data.drop(0, axis=1).T.reset_index(drop=True)
                 name = 'ALL'
                 color_number = "-"
                 plot_line_list = []
                 for i in range(0, len(group_list)):
                     plot_line = ax.plot(X_axis, new_data[new_data[0]==group_list[i]].drop(0, axis=1).T.reset_index(drop=True), color='{}'.format(subtitle_and_color[round(group_list[i])][0]), label=subtitle_and_color[round(group_list[i])][1])
+                    # plot_line = ax.plot(X_axis, new_data[new_data[0]==group_list[i]].drop(0, axis=1).T.reset_index(drop=True), color='{}'.format(subtitle_and_color[round(group_list[i])][0]), label=subtitle_and_color[round(group_list[i])][1])
                     plot_line_list.append(plot_line[0])
                 ax.legend(plot_line_list, plot_line_list)
             else :
@@ -187,7 +187,7 @@ def visualizer(file_name, # include .csv -> sample.csv
             data_time_lenght = len(process_data)
             n_rythm = int(-(-(data_time_lenght/(60/sampling_period))//24))
             X_max = int(n_rythm*24)
-            original_lenght = len(all_data.T)
+            original_lenght = len(new_data.drop(0, axis=1))
             data_lenght = len(process_data.T)
             data_percentage = round(data_lenght/original_lenght*100, 1)
 
@@ -206,9 +206,54 @@ def visualizer(file_name, # include .csv -> sample.csv
         plt.show()
 
 
-    def all_plot(X_axis, new_data, all_data, data_name, Yaxis):
-        F_max = np.amax(np.amax(new_data))
-        Y_max = -(-F_max//1000)*1000
+    def overlap_write(overlap_dict, X_axis, shaped_data, data_name, Yaxis, column_number, x_axis_title, y_axis_title):
+        group_list = sorted(list(set(shaped_data[0])))
+        if yaxis_percentage_switch == 1:
+            new_data = percentage_data_transfer(shaped_data)
+            Y_max = 100
+        else :
+            new_data = shaped_data
+            F_max = np.amax(np.amax(new_data.drop(0, axis=1)))
+            Y_max = -(-F_max//1000)*1000
+        fig = plt.figure(figsize=(column_number*ov_width, -(-(len(group_list)+column_number)//column_number)*ov_length))
+        # for I in range (0, len(group_list)+1, 1):
+        count = 0
+        print("column_number = {0}".format(column_number))
+        for title in overlap_dict.keys():
+            ax =  fig.add_subplot(-(-(len(group_list)+column_number)//column_number), column_number, count+1)
+            plot_line_list = []
+            for i in overlap_dict[title]:
+                plot_line = ax.plot(X_axis, new_data[new_data[0]==i].drop(0, axis=1).T.reset_index(drop=True), color='{}'.format(subtitle_and_color[i][0]), label=subtitle_and_color[i][1])
+                plot_line_list.append(plot_line[0])
+            ax.legend(plot_line_list, plot_line_list)
+            #変数セット
+            data_time_lenght = len(new_data.drop(0, axis=1).T)
+            n_rythm = int(-(-(data_time_lenght/(60/sampling_period))//24))
+            X_max = int(n_rythm*24)
+            each_title = '{0}'.format(title)
+            ax.set_title(each_title)
+            ax.set_xticks(np.linspace(0, X_max, n_rythm+1))
+            ax.set_xticks(np.linspace(0, X_max, n_rythm*4+1), minor=True)
+            ax.set_xlabel(x_axis_title)
+            if Yaxis == "Y shared":
+                ax.set_ylim(0, Y_max)
+            ax.set_ylabel(y_axis_title)
+            ax.grid(axis="both")
+            count = count + 1
+        fig.tight_layout()
+        plt.savefig( "overlap_col_plot.jpg")
+        plt.show()
+        return
+
+
+    def all_plot(X_axis, shaped_data, data_name, Yaxis):
+        if yaxis_percentage_switch == 1:
+            new_data = percentage_data_transfer(shaped_data)
+            Y_max = 100
+        else :
+            new_data = shaped_data
+            F_max = np.amax(np.amax(new_data.drop(0, axis=1)))
+            Y_max = -(-F_max//1000)*1000
         fig = plt.figure(figsize=(a_column*a_width, -(-new_data.shape[0]//a_column)*a_length))
         print("file_from : {}".format(file_from))
         for i in range(1, new_data.shape[0]+1):
@@ -263,4 +308,45 @@ def visualizer(file_name, # include .csv -> sample.csv
         plt.show()
 
 
+    """
+    処理分岐
+    """
+
+    def router(file_path, file_name, column_number):
+        if file_name[-4:] != ".csv":
+            print("ERROR ->\nPlease use csv file.")
+            sys.exit()
+        row_data = pd.read_csv("{0}{1}".format(file_path, file_name), engine="python", encoding="utf-8_sig")
+        try:
+            shaped_data = row_data.drop('Unnamed: 0', axis=1).T
+            X_axis = round(row_data["Unnamed: 0"].iloc[1:].reset_index(drop=True).astype(float), 4)
+        except KeyError:
+            shaped_data = row_data.drop('Time', axis=1).T
+            X_axis = round(row_data['Time'].iloc[1:].reset_index(drop=True).astype(float), 4)
+        finally:
+            if y_axis_share_switch == 0:
+                Yaxis = "Not shared"
+            else :
+                Yaxis = "Y shared"
+
+            if over_view_plot_switch == 1:
+                colored_overview_n_columns(X_axis, shaped_data, file_name, Yaxis, overwrite_column_number, x_axis_title, y_axis_title)
+            else:
+                pass
+
+            if len(overlap_dict) > 0:
+                overlap_write(overlap_dict, X_axis, shaped_data, file_name, Yaxis, column_number, x_axis_title, y_axis_title)
+            else :
+                pass
+
+            if all_plot_switch == 1:
+                all_plot(X_axis, shaped_data, file_name, Yaxis)
+            else:
+                pass
+
+
+
+    """
+    実行関数
+    """
     router(file_path, file_name, column_number)
